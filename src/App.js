be40b158da2e165a4f09030de5040370fa6ba6cf
unknown <abusiddick84@gmail.com>
2022-02-11 21:05:35 +0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: "Dr.Mahfuz",
+    job: "Singer"
+  }
+  var person2 = {
+    name: "Siddik",
+    job: 'Student'
+  }
+  var style = {
+    color:'red',
+    backgroundColor: 'yellow'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +21,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>My heading: {(2+2)*4}</h1>
+        <h1 className='' style={style}>Person: {person.name + " " + person.job}</h1>
+        <h3 style={{backgroundColor:'cyan',color:'yellow'}}>Person2: {person2.name + " " + person2.job}</h3>
+        <p>My first React Paragraph</p>
         <a
           className="App-link"
           href="https://reactjs.org"
